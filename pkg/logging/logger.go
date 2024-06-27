a29319c20bb320fafa631e9e7bb7c8aaba42b7df
rydelll <73040581+rydelll@users.noreply.github.com>
2024-06-27 12:14:17 -0400

    Update and rename logging package

---
+++ pkg/logging/logger.go
@@ -0,0 +1,80 @@
+package logging
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"sync"
+)
+
+// contextKey is a private string type to prevent collisions in the context map.
+type contextKey string
+
+const (
+	// loggerKey points to the value in the context where the logger is stored.
+	loggerKey = contextKey("logger")
+	// defaultLogLevel sets the default logger verbosity level.
+	defaultLogLevel = slog.LevelInfo
+	// defaultLogJSON sets the default logger output format.
+	defaultLogJSON = true
+)
+
+var (
+	// defaultLogger is the default logger. It is initialized once per package
+	// include when calling [DefaultLogger].
+	defaultLogger     *slog.Logger
+	defaultLoggerOnce sync.Once
+)
+
+// NewLogger creates a [slog.Logger] with the given verbosity and output format.
+func NewLogger(level slog.Level, json bool) *slog.Logger {
+	var handler slog.Handler
+	if json {
+		handler = slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: level},
+		)
+	} else {
+		handler = slog.NewTextHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: level},
+		)
+	}
+	return slog.New(handler)
+}
+
+// DefaultLogger returns the default logger for the package.
+func DefaultLogger() *slog.Logger {
+	defaultLoggerOnce.Do(func() {
+		defaultLogger = NewLogger(defaultLogLevel, defaultLogJSON)
+	})
+	return defaultLogger
+}
+
+// WithLogger creates a new context with the provided logger attached.
+func WithLogger(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+
+// FromContext returns the logger stored in a context. If no such context
+// exists, a default logger is returned.
+func FromContext(ctx context.Context) *slog.Logger {
+	if logger, ok := ctx.Value(loggerKey).(*slog.Logger); ok {
+		return logger
+	}
+	return DefaultLogger()
+}
+
+// SlogLevel converts the given string to the appropriate log level.
+func SlogLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "warning":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
