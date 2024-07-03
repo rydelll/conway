b7f1edb7f116bec655f6adbd5bbc1d855813c285
rydelll <73040581+rydelll@users.noreply.github.com>
2024-07-03 01:20:52 -0400

    Fix server logging so all logs contain server address

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -26,7 +26,6 @@ type Server struct {
 // New creates a server with optional configuration.
 func New(logger *slog.Logger, handler http.Handler, port int, opts ...Option) *Server {
 	s := &Server{
-		logger:          logger,
 		shutdownTimeout: defaultShutdownTimeout,
 	}
 
@@ -38,6 +37,10 @@ func New(logger *slog.Logger, handler http.Handler, port int, opts ...Option) *S
 		IdleTimeout:  defaultIdleTimeout,
 		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelWarn),
 	}
+
+	s.logger = logger.WithGroup("server").With(
+		slog.String("addr", s.server.Addr),
+	)
 
 	// apply optional configuration
 	for _, opt := range opts {
@@ -54,21 +57,20 @@ func New(logger *slog.Logger, handler http.Handler, port int, opts ...Option) *S
 // Once it has been stopped it is NOT safe for reuse.
 func (s *Server) ListenAndServe(ctx context.Context) error {
 	shutdownErrorChan := make(chan error, 1)
-	logger := s.logger.WithGroup("server").With(slog.String("addr", s.server.Addr))
 
 	go func() {
 		<-ctx.Done()
 
-		logger.Debug("shutdown signal recieved")
+		s.logger.Debug("shutdown signal recieved")
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
 		defer cancel()
 
-		logger.Debug("shutting down", slog.String("timeout", s.shutdownTimeout.String()))
+		s.logger.Debug("shutting down", slog.String("timeout", s.shutdownTimeout.String()))
 		shutdownErrorChan <- s.server.Shutdown(shutdownCtx)
 	}()
 
-	logger.Info("listening and serving HTTP")
-	defer logger.Info("stopped listening and serving HTTP")
+	s.logger.Info("listening and serving HTTP")
+	defer s.logger.Info("stopped listening and serving HTTP")
 
 	err := s.server.ListenAndServe()
 	if !errors.Is(err, http.ErrServerClosed) {
